6325a36bf45105cfd18421de835e3827f0cd7bdc
cloud-dev-user <70812553+cloud-dev-user@users.noreply.github.com>
2024-05-17 19:08:27 +0530

    Update deployment.py

--- deployment.py
+++ deployment.py
@@ -1,3 +1,15 @@
 THis is deployment script 
 
 it is going to export verrsion of eagle, figma, tele from deployment_version.txt 
+
+take the version of app: eagle 
+
+downalod from artifact repo 
+
+deploy it 
+
+in case of rollabck 
+
+take the rollback version and deploy it 
+
+notify the results the team 
